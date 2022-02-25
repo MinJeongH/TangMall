2946688d6057c7acd8c6b7c212b811f990cb7798
MinJengH <gkaalswjd2@gmail.com>
2022-02-25 15:24:21 +0900

    메인 페이지 윤곽 잡아둠

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,16 @@ import React from "react";
 import "./App.scss";
 import Card from "./components/layout/card";
 import ItemDetail from "./components/detail/item_detail";
-import Commercial from "./components/layout/commercial";
 import Header from "./components/layout/header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./components/main/main";
 import Info from "./components/layout/info";
+import Ads from "./components/layout/ads";
 
 const routes = [
   {
     path: "/",
-    component: (
-      <Card>
-        <Main />
-      </Card>
-    ),
+    component: <Main />,
   },
   {
     path: "/detail",
@@ -31,7 +27,7 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <Commercial />
+      <Ads />
       <BrowserRouter>
         <Routes>
           {routes.map((route) => (
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import Card from "../layout/card";
+import "./main.scss";
 
-const Main = () => <h1>main</h1>;
+const Main = () => {
+  return (
+    <section className="main">
+      <div className="close_box">
+        <Card>
+          <h1 className="title">마감임박</h1>
+        </Card>
+      </div>
+      <div className="popular_box">
+        <Card>
+          <h1 className="title">인기경매</h1>
+        </Card>
+      </div>
+      <div className="new_box">
+        <Card>
+          <h1 className="title">최신경맨</h1>
+        </Card>
+      </div>
+    </section>
+  );
+};
 
 export default Main;
